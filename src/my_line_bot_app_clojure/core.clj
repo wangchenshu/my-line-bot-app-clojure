ac7fc0cd69f8f7affed91783e89ecf0d408c843e
Walter Wang <csw.dawm@gmail.com>
2016-06-12 11:27:37 +0800

    Add message.clj

--- src/my_line_bot_app_clojure/core.clj
+++ src/my_line_bot_app_clojure/core.clj
@@ -3,12 +3,20 @@
             [compojure.route :as route]
             [ring.adapter.jetty :as jetty]
             [org.httpkit.client :as http]
-            [clojure.data.json :as json]))                                        
+            [clojure.data.json :as json]
+            [clojure.string :as string]
+            [my-line-bot-app-clojure.message :as message]))                                        
 
 (def config (load-file "config.clj"))
+
+(def headers {"Content-Type" "application/json",
+              "X-Line-ChannelID" (config :channel-id),
+              "X-Line-ChannelSecret" (config :channel-secret),
+              "X-Line-Trusted-User-With-ACL" (config :channel-mid)})
+
 (def options {:timeout 200             ; ms
               :user-agent "User-Agent-string"
-              :headers {"Content-Type" "application/json", "X-Line-ChannelID" (config :channel-id), "X-Line-ChannelSecret" (config :channel-secret), "X-Line-Trusted-User-With-ACL" (config :channel-mid)}})
+              :headers headers})
   
 (defn callback-handler [req]
   (let [res (json/read-str (slurp(req :body)) :key-fn keyword)] 
@@ -17,8 +25,15 @@
                      first
                      :content)
           from (content :from)
-          from-text (content :text)
-          send-body {:to (list from) :toChannel (config :event-to-channel-id) :eventType (config :event-type) :content {:toType 1 :contentType 1 :text from-text}}]      
+          send-text (message/send-text :default)          
+          to-text (content :text);(string/join "\n" send-text)
+          send-body {:to (list from)
+                     :toChannel (config :event-to-channel-id)
+                     :eventType (config :event-type)
+                     :content {:toType 1
+                               :contentType (message/content-type :text)
+                               :text to-text}}]
+
       (let [resp1 (http/post (str (config :channel-url) (config :event-path)) (assoc options :body (json/write-str send-body)))]        
         (println "Response 1's status: " (:status @resp1))))))
 
