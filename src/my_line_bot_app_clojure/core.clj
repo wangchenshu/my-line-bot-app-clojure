89091fc25aae868df10a5f49f2990844399d06fd
Walter Wang <csw.dawm@gmail.com>
2016-06-14 22:58:08 +0800

    Add debug

--- src/my_line_bot_app_clojure/core.clj
+++ src/my_line_bot_app_clojure/core.clj
@@ -7,12 +7,13 @@
             [org.httpkit.client :as http]
             [my-line-bot-app-clojure.message :as message]))                                        
 
+(def listening-port 3000)
 (def config (load-file "config.clj"))
 (def headers {"Content-Type" "application/json",
               "X-Line-ChannelID" (config :channel-id),
               "X-Line-ChannelSecret" (config :channel-secret),
               "X-Line-Trusted-User-With-ACL" (config :channel-mid)})
-(def options {:timeout 1000 :headers headers})
+(def options {:timeout 100 :headers headers})
 
 (defn callback-handler [req]
   (let [res (json/read-str (slurp(req :body)) :key-fn keyword)] 
@@ -23,10 +24,10 @@
           send-body (message/create-text-message (content :from) (content :text))]
       (let [resp (http/post (str (config :channel-url) (config :event-path))
                             (assoc options :body (json/write-str send-body)))]
-        (:body @resp)))))
+        (println (:body @resp))))))
 
 (defroutes main-routes
   (POST "/callback" req (callback-handler req)))
 
 (defn -main []
-  (jetty/run-jetty main-routes {:port 3000}))
+  (jetty/run-jetty main-routes {:port listening-port}))
